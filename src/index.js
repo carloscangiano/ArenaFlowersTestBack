fdbe51ab8d806ed2df8f01b86f026262701b950d
Carlos Cangiano <carlos.cangiano@ross.com.ar>
2021-05-13 20:29:59 -0300

    Heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const app = require('./app');
 const logger = require('./utils/logger');
 const { configSV } = require('./config/configSV');
+require("dotenv").config();
 
-app.listen(configSV.port, configSV.hostname, () => {
-  logger.info(`Server running at http://${configSV.hostname}:${configSV.port}/`);
-}); 
+app.listen(configSV.port, () => {
+  logger.info("*Welcome to X-Men analyze DNA Api*");
+  const uriSV = process.env.npm_config_production
+    ? process.env.HEROKU_URI
+    : `http://${configSV.hostname}:${configSV.port}`;
+  logger.info(`Server running at ${uriSV}`);
+});
